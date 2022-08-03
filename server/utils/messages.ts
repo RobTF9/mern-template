09e1d5766664f9ebc4e072a3e0233eac579d1bdf
Rob <robtf9@icloud.com>
2022-08-03 09:39:05 +0100

    auth messages

--- server/utils/messages.ts
+++ server/utils/messages.ts
@@ -11,9 +11,19 @@ const succesMessage = (message: string) => ({
 })
 
 export const SUCCESS_MESSAGE = {
-  ITEM_CREATED: succesMessage('Item created'),
+  RESOURCE_CREATED: (resource: string) => succesMessage(`${resource} created`),
+  SUCCESSFUL_SIGN_IN: succesMessage('Successfully signed in'),
+  SUCCESSFUL_SIGN_UP: succesMessage('Successfully signed up'),
+  SUCCESSFUL_SIGN_OUT: succesMessage('Successfully signed out'),
+  AUTHORIZED: errorMessage('Authorized'),
 }
 
 export const ERROR_MESSAGE = {
   INTERNAL_SERVER: errorMessage('Internal server error'),
+  NO_EMAIL_OR_PASSWORD: errorMessage('Provide an email and password'),
+  NO_EMAIL_OR_USERNAME_OR_PASSWORD: errorMessage('Provide an email, username and password'),
+  INVALID_EMAIL_OR_PASSWORD: errorMessage('Invalild email and password '),
+  EMAIL_IN_USE: errorMessage('Email address is already in use'),
+  USERNAME_IN_USE: errorMessage('Username is already in use'),
+  NOT_AUTHORIZED: errorMessage('Not authorized'),
 }
