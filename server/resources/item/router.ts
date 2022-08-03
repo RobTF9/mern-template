3b77e0e2295996e88fe84a795effb95de4b7032f
Rob <robtf9@icloud.com>
2022-08-03 14:34:18 +0100

    absract controllers

--- server/resources/item/router.ts
+++ server/resources/item/router.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
-import { create, deleteOne, readAll, readOne, update } from './controller'
+import controllers from './controller'
 
 const router = Router()
 
-router.route('/').post(create).get(readAll)
-router.route('/:id').put(update).get(readOne).delete(deleteOne)
+router.route('/').post(controllers.create).get(controllers.readAll)
+router.route('/:id').put(controllers.update).get(controllers.readOne).delete(controllers.deleteOne)
 
 export default router
---
+++ server/utils/crud.ts
@@ -0,0 +1,75 @@
+import { RequestHandler } from 'express'
+import { Model } from 'mongoose'
+import { SUCCESS_MESSAGE } from './messages'
+
+function crudControllers<T>(model: Model<T>, collection: string) {
+  const create: RequestHandler = async (req, res, next) => {
+    try {
+      const item = await model.create({
+        ...req.body,
+        createdBy: req.session.user,
+      })
+      return res
+        .status(200)
+        .json({ data: item, message: SUCCESS_MESSAGE.RESOURCE_CREATED(collection) })
+    } catch (error) {
+      return next(error)
+    }
+  }
+
+  const readAll: RequestHandler = async (_, res, next) => {
+    try {
+      const items = await model.find()
+      return res.status(200).json({ data: items })
+    } catch (error) {
+      return next(error)
+    }
+  }
+
+  const readOne: RequestHandler = async (req, res, next) => {
+    try {
+      const items = await model.findById(req.params.id).lean().exec()
+      return res.status(200).json({ data: items })
+    } catch (error) {
+      return next(error)
+    }
+  }
+
+  const update: RequestHandler = async (req, res, next) => {
+    try {
+      const item = await model.findByIdAndUpdate(
+        req.params.id,
+        {
+          ...req.body,
+        },
+        { new: true }
+      )
+      return res
+        .status(201)
+        .json({ data: item, message: SUCCESS_MESSAGE.RESOURCE_UPDATED(collection) })
+    } catch (error) {
+      return next(error)
+    }
+  }
+
+  const deleteOne: RequestHandler = async (req, res, next) => {
+    try {
+      const item = await model.findByIdAndDelete(req.params.id).lean().exec()
+      return res
+        .status(200)
+        .json({ data: item, message: SUCCESS_MESSAGE.RESOURCE_DELETED(collection) })
+    } catch (error) {
+      return next(error)
+    }
+  }
+
+  return {
+    create,
+    readAll,
+    readOne,
+    update,
+    deleteOne,
+  }
+}
+
+export default crudControllers
