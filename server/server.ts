65f3d24d74a0f3bb226a706807e6494b6aa08e52
Rob <robtf9@icloud.com>
2022-08-03 08:02:39 +0100

    add error handler

--- server/server.ts
+++ server/server.ts
@@ -1,34 +1,37 @@
-import express from "express";
-import { json, urlencoded } from "body-parser";
-import morgan from "morgan";
-import router from "./resources/item/router";
-import path from "path";
-import dotenv from "dotenv";
-import connect from "./db/connect";
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+import router from './resources/item/router'
+import path from 'path'
+import dotenv from 'dotenv'
+import connect from './db/connect'
+import errorHandler from './utils/error'
 
-dotenv.config();
+dotenv.config()
 
-export const app = express();
-const port = process.env.PORT || 3000;
+export const app = express()
+const port = process.env.PORT || 3000
 
-app.use(json());
-app.use(urlencoded({ extended: true }));
-app.use(morgan("dev"));
-app.set("trust proxy", 1);
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+app.set('trust proxy', 1)
 
-app.use("/api", router);
+app.use('/api', router)
 
-const clientPath = path.join(__dirname, "..", "client");
-app.use(express.static(clientPath));
-app.use("*", express.static(clientPath));
+const clientPath = path.join(__dirname, '..', 'client')
+app.use(express.static(clientPath))
+app.use('*', express.static(clientPath))
+
+app.use(errorHandler)
 
 async function startServer(): Promise<void> {
   try {
-    connect();
-    app.listen(port, () => console.log(`Server running on ${port}`));
+    connect()
+    app.listen(port, () => console.log(`Server running on ${port}`))
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 }
 
-export default startServer;
+export default startServer
