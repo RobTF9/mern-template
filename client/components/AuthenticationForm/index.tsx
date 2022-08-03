123b07b8d67a8234bd9cc446b3e0e9024e6081c0
Rob <robtf9@icloud.com>
2022-08-03 15:31:41 +0100

    layout

--- client/components/AuthenticationForm/index.tsx
+++ client/components/AuthenticationForm/index.tsx
@@ -21,7 +21,7 @@ const AuthenticationForm: React.FC<Props> = ({ details, submit, onChange }) => {
     >
       {Object.entries(details).map(([key, value]) => (
         <label>
-          {key}
+          <p>{key}</p>
           <input
             onChange={onChange}
             value={value}
--- client/router/index.tsx
+++ client/router/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Layout from '../components/Layout'
 import { useAuthContext } from '../context/auth'
 import Authenticated from './authenticated'
 import NotAuthenticated from './notAuthenticated'
 
 const Router = () => {
   const { authenticated } = useAuthContext()
-  return authenticated ? <Authenticated /> : <NotAuthenticated />
+  return <Layout>{authenticated ? <Authenticated /> : <NotAuthenticated />}</Layout>
 }
 
 export default Router
