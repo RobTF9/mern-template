7e8a22c3f5ccb8ec1ee3e430aa365e345ed89fa3
Rob <robtf9@icloud.com>
2022-08-03 10:15:05 +0100

    no auth router

--- client/router/index.tsx
+++ client/router/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import { useAuthContext } from '../context/auth'
 import Authenticated from './authenticated'
 import NotAuthenticated from './notAuthenticated'
 
 const Router = () => {
   const { authenticated } = useAuthContext()
-  return authenticated ? <Authenticated /> : <NotAuthenticated />
+  return <BrowserRouter>{authenticated ? <Authenticated /> : <NotAuthenticated />}</BrowserRouter>
 }
 
 export default Router
