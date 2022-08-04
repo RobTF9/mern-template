edc7d6a8cb1ddbbda003fc22537890b972f86e19
Rob <robtf9@icloud.com>
2022-08-04 09:09:07 +0100

    item list layout

--- client/components/ItemList/styles.ts
+++ client/components/ItemList/styles.ts
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 export const ItemListWrapper = styled.ul`
   list-style-type: none;
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+  display: flex;
+  flex-direction: column;
   gap: 1rem;
   margin-top: 2rem;
 
@@ -11,5 +11,12 @@ export const ItemListWrapper = styled.ul`
     padding: 2rem;
     box-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.1);
     border-radius: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  li button {
+    background-color: var(--error);
   }
 `
