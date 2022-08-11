4adeaeada67f0d5cfe289cd5836726ea56accb87
RobTF9 <robtf9@icloud.com>
2022-08-11 15:42:35 -0400

    can console log socket

--- client/hooks/useSocket.ts
+++ client/hooks/useSocket.ts
@@ -1,5 +1,20 @@
+import { useEffect, useRef } from 'react'
+import { Socket } from 'socket.io'
 import { io } from 'socket.io-client'
 
-function useSocket() {}
+function useSocket() {
+  const socketRef = useRef<Socket | null>(null)
+
+  useEffect(() => {
+    console.log('in hook')
+    if (!socketRef.current) {
+      socketRef.current = io()
+      console.log('in condition')
+      return
+    }
+  }, [])
+
+  return socketRef
+}
 
 export default useSocket
--- client/router/pages/items.tsx
+++ client/router/pages/items.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ItemForm from '../../components/ItemForm'
 import ItemList from '../../components/ItemList'
 import useResource from '../../data/useResource'
+import useSocket from '../../hooks/useSocket'
 
 const Items = () => {
   const resource = useResource<ItemResource, ItemInterface>('/api/item')
   const { items, itemsLoading, create, createLoading, update, del } = resource
+  const socket = useSocket()
+
+  useEffect(() => {
+    console.log(socket)
+  }, [socket])
 
   if (itemsLoading || createLoading) return <h1>Loading...</h1>
 
