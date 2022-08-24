bdff9784deb516a2f7c3003dc25f6c6f01b5eeb3
RobTF9 <robtf9@icloud.com>
2022-08-24 09:43:01 +0100

    list page

--- client/router/pages/list.tsx
+++ client/router/pages/list.tsx
@@ -1,14 +1,30 @@
 // create a new socket that cruds items in a room equivalent to a active list id
 // push active list to session
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import useSocket from '../../hooks/useSocket'
 
 const List = () => {
   const { id } = useParams()
   const { emitter } = useSocket(id || '')
+  const [newItem, setNewItem] = useState('')
 
-  return <div>List</div>
+  return (
+    <div>
+      <h2>List {id}</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault()
+          emitter('create', newItem)
+        }}
+      >
+        <label>
+          New item{' '}
+          <input type="text" value={newItem} onChange={(event) => setNewItem(event.target.value)} />
+        </label>
+      </form>
+    </div>
+  )
 }
 
 export default List
