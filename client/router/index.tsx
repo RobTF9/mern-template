2a34a86561eaff120b144fa3845de562bc29ca6e
RobTF9 <robtf9@icloud.com>
2022-11-25 21:02:27 +0000

    loading

--- client/router/index.tsx
+++ client/router/index.tsx
@@ -6,6 +6,7 @@ import NotAuthenticated from './notAuthenticated'
 
 const Router = () => {
   const { authenticated } = useAuthContext()
+  if (authenticated == null) return <h1>Loading...</h1>
   return <Layout>{authenticated ? <Authenticated /> : <NotAuthenticated />}</Layout>
 }
 
